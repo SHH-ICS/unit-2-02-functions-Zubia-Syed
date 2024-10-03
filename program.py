1ef633b140ca2f41d1a40cf712fad00a9cd16499
Zubia-Syed <Zubia.syed@stu.ocsb.ca>
2024-10-03 14:23:25 +0000

    2-02

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction(Hello,world):
-  #my function
-  Print("Hello,world")
+def myFunction():
+   #my function
+  x= "Hello world!"
+  Print(x)
   
